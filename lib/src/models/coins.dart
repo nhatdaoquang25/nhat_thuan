245bb4c5c4f1f7e21070c42bce7827bf2d73c492
Dao Quang Nhat <nhatdaoquang25@gmail.com>
2021-11-15 11:09:34 +0700

    fix models

--- lib/src/models/coins.dart
+++ lib/src/models/coins.dart
@@ -1,47 +1,40 @@
 class Coins {
-  String? id;
-  String? symbol;
-  String? name;
+  String id;
+  String symbol;
+  String name;
   String? image;
-  double? currentPrice;
+  int? currentPrice;
   double? high24H;
   double? low24H;
   double? priceChange24H;
   double? priceChangePercentage24H;
-  double? marketCap;
-  double? circulatingSupply;
-  double? totalSupply;
-  double? maxSupply;
-  String? description;
 
-  Coins(
-      {this.id,
-      this.symbol,
-      this.name,
-      this.image,
-      this.currentPrice,
-      this.high24H,
-      this.low24H,
-      this.priceChange24H,
-      this.marketCap,
-      this.circulatingSupply,
-      this.totalSupply,
-      this.maxSupply,
-      this.description});
+  Coins({
+    required this.id,
+    required this.symbol,
+    required this.name,
+    this.image,
+    this.currentPrice,
+    required this.high24H,
+    required this.low24H,
+    required this.priceChange24H,
+    required this.priceChangePercentage24H,
+  });
 
   factory Coins.fromJson(Map<String, dynamic> json) => Coins(
-        id: json["id"] ?? 'None',
-        symbol: json["symbol"] ?? 'None',
-        name: json["name"] ?? "None",
-        image: json["image"] ?? json["image"]["small"],
-        currentPrice: json["current_price"]["usd"].toDouble() ?? 'None',
-        marketCap: json["market_cap"]["usd"] ?? 'None',
-        high24H: json["high_24h"]["usd"].toDouble() ?? 'None',
-        low24H: json["low_24h"]["usd"].toDouble() ?? 'None',
-        priceChange24H: json["price_change_24h"].toDouble() ?? 'None',
-        circulatingSupply: json["circulating_supply"].toDouble() ?? 'None',
-        totalSupply: json["total_supply"].toDouble() ?? 'None',
-        maxSupply: json["max_supply"].toDouble() ?? 'None',
-        description: json["description"]["en"] ?? 'None',
+        id: json["id"],
+        symbol: json["symbol"],
+        name: json["name"],
+        image: json["image"],
+        currentPrice:
+            json["current_price"] == null ? 0 : json["current_price"].toInt(),
+        high24H: json["high_24h"] == null ? 0 : json["high_24h"].toDouble(),
+        low24H: json["low_24h"] == null ? 0 : json["low_24h"].toDouble(),
+        priceChange24H: json["price_change_24h"] == null
+            ? 0
+            : json["price_change_24h"].toDouble(),
+        priceChangePercentage24H: json["price_change_percentage_24h"] == null
+            ? 0
+            : json["price_change_percentage_24h"].toDouble(),
       );
 }
