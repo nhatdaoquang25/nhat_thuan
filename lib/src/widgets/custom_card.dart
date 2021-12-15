ae3b85cf9bdd698fd557b00c7c4941205bd12aff
Dao Quang Nhat <nhatdaoquang25@gmail.com>
2021-12-15 10:44:31 +0700

    test

--- lib/src/widgets/custom_card.dart
+++ lib/src/widgets/custom_card.dart
@@ -103,7 +103,7 @@ class CustomCard extends StatelessWidget {
                       const Spacer(),
                       Text(
                         '\$$currentPrice',
-                        style: const TextStyle(fontSize: 16),
+                        style: const TextStyle(fontSize: 17),
                       )
                     ],
                   ))
